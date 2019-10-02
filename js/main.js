74926fbed9465d0e0de3d9bc933999194341a616
JhonySmith <evesnin68@gmail.com>
2019-10-02 18:02:00 +0300

    Style elements

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 var writeUs = document.getElementById('write-us');
 var closePopup = document.getElementById('close');
+var search = document.getElementById('site-search');
+var searchButton = document.getElementById('search-button');
 
 writeUs.onclick = function () {
-    document.getElementById('popup-write-us').style.display = "flex";
-    return false;
+  document.getElementById('popup-write-us').style.display = "flex";
+  return false;
 }
 
 closePopup.onclick = function () {
-    document.getElementById('popup-write-us').style.display = "none";
+  document.getElementById('popup-write-us').style.display = "none";
 }
+
+search.onfocus = function () {
+  document.getElementById('search-form').style.borderBottom = "2px solid black";
+  document.getElementById('search-button').style.display = 'block';
+}
+
+search.onblur = function () {
+  document.getElementById('search-form').style.borderBottom = "2px solid transparent";
+  document.getElementById('search-button').style.display = 'none';
+}
+
