81859d71fd5392444c9f78c67fec155f97012d53
JhonySmith <evesnin68@gmail.com>
2019-10-03 17:31:28 +0300

    style

--- js/main.js
+++ js/main.js
@@ -1,24 +1,35 @@
-var writeUs = document.getElementById('write-us');
-var closePopup = document.getElementById('close');
-var search = document.getElementById('site-search');
-var searchButton = document.getElementById('search-button');
+var writeUs = document.getElementById("write-us");
+var closePopup = document.getElementById("close");
+var fullMap = document.getElementById("full-map");
+var closeMap = document.getElementById("close-map");
+var search = document.getElementById("site-search");
+var searchButton = document.getElementById("search-button");
 
-writeUs.onclick = function () {
-  document.getElementById('popup-write-us').style.display = "flex";
+writeUs.onclick = function() {
+  document.getElementById("popup-write-us").style.display = "flex";
   return false;
-}
+};
 
-closePopup.onclick = function () {
-  document.getElementById('popup-write-us').style.display = "none";
-}
+closePopup.onclick = function() {
+  document.getElementById("popup-write-us").style.display = "none";
+};
 
-search.onfocus = function () {
-  document.getElementById('search-form').style.borderBottom = "2px solid black";
-  document.getElementById('search-button').style.display = 'block';
-}
+fullMap.onclick = function() {
+  document.getElementById("popup-full-map").style.display = "block";
+  return false;
+};
 
-search.onblur = function () {
-  document.getElementById('search-form').style.borderBottom = "2px solid transparent";
-  document.getElementById('search-button').style.display = 'none';
-}
+closeMap.onclick = function() {
+  document.getElementById("popup-full-map").style.display = "none";
+};
 
+search.onfocus = function() {
+  document.getElementById("search-form").style.borderBottom = "2px solid black";
+  document.getElementById("search-button").style.display = "block";
+};
+
+search.onblur = function() {
+  document.getElementById("search-form").style.borderBottom =
+    "2px solid transparent";
+  document.getElementById("search-button").style.display = "none";
+};
