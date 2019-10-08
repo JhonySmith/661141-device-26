e38eaf539d8656b9f95f18fff3db30ff116545c8
JhonySmith <evesnin68@gmail.com>
2019-10-08 20:03:35 +0300

    js, drop menu

--- js/main.js
+++ js/main.js
@@ -13,23 +13,29 @@ var slideOneChecked = document.querySelector("#bigest-monopod");
 var slideTwoChecked = document.querySelector("#fitness-bracelet");
 var slideThreeChecked = document.querySelector("#quadrocopter");
 
-slideOneChecked.addEventListener("change", function() {
-  slideOne.classList.remove("hidden");
-  slideTwo.classList.add("hidden");
-  slideThree.classList.add("hidden");
-});
+if (slideOneChecked) {
+  slideOneChecked.addEventListener("change", function() {
+    slideOne.classList.remove("hidden");
+    slideTwo.classList.add("hidden");
+    slideThree.classList.add("hidden");
+  });
+}
 
-slideTwoChecked.addEventListener("change", function() {
-  slideOne.classList.add("hidden");
-  slideTwo.classList.remove("hidden");
-  slideThree.classList.add("hidden");
-});
+if (slideTwoChecked) {
+  slideTwoChecked.addEventListener("change", function() {
+    slideOne.classList.add("hidden");
+    slideTwo.classList.remove("hidden");
+    slideThree.classList.add("hidden");
+  });
+}
 
-slideThreeChecked.addEventListener("change", function() {
-  slideOne.classList.add("hidden");
-  slideTwo.classList.add("hidden");
-  slideThree.classList.remove("hidden");
-});
+if (slideThreeChecked) {
+  slideThreeChecked.addEventListener("change", function() {
+    slideOne.classList.add("hidden");
+    slideTwo.classList.add("hidden");
+    slideThree.classList.remove("hidden");
+  });
+}
 
 var dropMenu = document.querySelector(".drop-head");
 var dropMenuList = document.querySelector(".drop-menu-list");
@@ -52,41 +58,55 @@ var delivery = document.querySelector(".delivery");
 var warranty = document.querySelector(".warranty");
 var credit = document.querySelector(".credit");
 
-deliveryRadio.addEventListener("change", function() {
-  delivery.classList.remove("hidden");
-  warranty.classList.add("hidden");
-  credit.classList.add("hidden");
-});
+if (deliveryRadio) {
+  deliveryRadio.addEventListener("change", function() {
+    delivery.classList.remove("hidden");
+    warranty.classList.add("hidden");
+    credit.classList.add("hidden");
+  });
+}
 
-warrantyRadio.addEventListener("change", function() {
-  delivery.classList.add("hidden");
-  warranty.classList.remove("hidden");
-  credit.classList.add("hidden");
-});
+if (warrantyRadio) {
+  warrantyRadio.addEventListener("change", function() {
+    delivery.classList.add("hidden");
+    warranty.classList.remove("hidden");
+    credit.classList.add("hidden");
+  });
+}
 
-creditRadio.addEventListener("change", function() {
-  delivery.classList.add("hidden");
-  warranty.classList.add("hidden");
-  credit.classList.remove("hidden");
-});
+if (creditRadio) {
+  creditRadio.addEventListener("change", function() {
+    delivery.classList.add("hidden");
+    warranty.classList.add("hidden");
+    credit.classList.remove("hidden");
+  });
+}
 
-writeUs.onclick = function() {
-  document.getElementById("popup-write-us").style.display = "flex";
-  return false;
-};
+if (writeUs) {
+  writeUs.onclick = function() {
+    document.getElementById("popup-write-us").style.display = "flex";
+    return false;
+  };
+}
 
-closePopup.onclick = function() {
-  document.getElementById("popup-write-us").style.display = "none";
-};
+if (closePopup) {
+  closePopup.onclick = function() {
+    document.getElementById("popup-write-us").style.display = "none";
+  };
+}
 
-fullMap.onclick = function() {
-  document.getElementById("popup-full-map").style.display = "block";
-  return false;
-};
+if (fullMap) {
+  fullMap.onclick = function() {
+    document.getElementById("popup-full-map").style.display = "block";
+    return false;
+  };
+}
 
-closeMap.onclick = function() {
-  document.getElementById("popup-full-map").style.display = "none";
-};
+if (closeMap) {
+  closeMap.onclick = function() {
+    document.getElementById("popup-full-map").style.display = "none";
+  };
+}
 
 search.onfocus = function() {
   document.getElementById("search-form").style.borderBottom = "2px solid black";
